9bafb514bc23d3bffa3099af2506ba694f0cb9cc
Filipesouza81 <filipesouza81@gmail.com>
2025-01-21 21:56:24 -0300

    update alterações

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Erro ao carregar o menu:", error));
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../../componentes/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        })
+        .catch(error => console.error("Erro ao carregar o rodapé:", error));
+});
